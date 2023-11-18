426177dd8c6c84cd7138408730c80d9656f1c255
Gesang Paudra J <g.paudra@gmail.com>
2023-11-18 14:39:39 +0700

    set judul berita to link

--- app/Views/home/berita.php
+++ app/Views/home/berita.php
@@ -3,21 +3,25 @@
     <div class="container">
         <div class="row mt-5">
             <div class="col-md-12 text-center">
-                <h2>Berita FORMACA Terbaru</h2>
-                <hr>
+                <h2>Berita FORMACA Terbaru</h2><br />
+
             </div>
             <?php foreach ($berita2 as $berita2) { ?>
             <div class="col-md-4">
                 <div class="card" style="margin-bottom: 20px;">
                     <img src="<?= base_url('assets/upload/image/' . $berita2['gambar']) ?>">
                     <div class="card-body">
-                        <h3><?= $berita2['judul_berita'] ?></h3>
+                        <h2>
+                            <a href="<?= base_url('berita/read/' . $berita2['slug_berita']) ?>">
+                                <?= $berita2['judul_berita'] ?>
+                            </a>
+                        </h2>
                         <p class="card-text">
                             <?= $berita2['ringkasan'] ?>
                         </p>
                         <hr>
                         <p>
-                            <a href="<?= base_url('berita/read/' . $berita2['slug_berita']) ?>" class="btn btn-success">
+                            <a href="<?= base_url('berita/read/' . $berita2['slug_berita']) ?>" class="appointment-btn">
                                 <i class="fa fa-chevron-right"></i> Baca...
                             </a>
                         </p>
