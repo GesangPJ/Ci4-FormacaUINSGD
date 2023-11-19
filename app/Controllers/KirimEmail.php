12b86c63e15eb317f71a6ee0f2d5004976eb7e80
Gesang Paudra J <g.paudra@gmail.com>
2023-11-19 17:17:15 +0700

    Create KirimEmail.php

---
+++ app/Controllers/KirimEmail.php
@@ -0,0 +1,50 @@
+<?php
+// app/Controllers/YourController.php
+
+namespace App\Controllers;
+
+class KirimEmail extends BaseController
+{
+    public function kirimpesan()
+    {
+        // Load the email library
+        $email = \Config\Services::email();
+
+        // Get form data
+        $name = $this->request->getPost('name');
+        $emailAddress = $this->request->getPost('email');
+        $subjek = $this->request->getPost('subjek');
+        $message = $this->request->getPost('message');
+
+        // Set email parameters
+        $email->setTo('info@formaca-uinsgdbandung.com');  // Update with your admin email
+        $email->setFrom('websiteformaca@formaca-uinsgdbandung.com', 'Website Formaca');
+        $email->setSubject($subjek);
+        $email->setMessage($message);
+        $email->SMTPDebug = 2;
+
+        // Send email
+    try {
+        if ($email->send()) {
+            // Email sent successfully
+            echo "Email sent successfully";
+            exit; // Add this line to stop further execution
+            // return redirect()->to('/')->with('success', 'Email sent successfully');
+        } else {
+            // Email sending failed
+            log_message('error', 'Email sending failed: ' . $email->printDebugger(['headers']));
+            echo "Failed to send email";
+            exit; // Add this line to stop further execution
+            // return redirect()->to('/')->with('error', 'Failed to send email');
+        }
+    } catch (\Exception $e) {
+        // Log any other exceptions
+        log_message('error', 'Exception: ' . $e->getMessage());
+        echo "An error occurred";
+        exit; // Add this line to stop further execution
+        // return redirect()->to('/')->with('error', 'An error occurred');
+    }
+        
+    }
+}
+?>
