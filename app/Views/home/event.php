656cbf75332b7bb5f7a52210f1dd3ac7bb74010a
Gesang Paudra J <g.paudra@gmail.com>
2023-11-21 15:14:29 +0700

    working custom event grid

--- app/Views/home/event.php
+++ app/Views/home/event.php
@@ -1,44 +1,36 @@
-<section id="contact" class="contact">
-    <div class="container">
-        <div class="row mt-5">
-            <div class="section-title">
-                <h2>EVENT FORMACA</h2>
-            </div>
-            <!--====== BLOG PART START ======-->
-            <section class="blog-area pb-5">
-                <div class="container">
-                    <div class="row justify-content-center">
-                        <?php foreach ($event as $event) { ?>
-                        <div class="col-lg-4 col-md-8 col-sm-10">
-                            <div class="single-blog blog-style-one">
-                                <div class="blog-image">
-                                    <a href="<?= base_url('berita/read/' . $event['slug_berita']) ?>"><img
-                                            src="<?= base_url('assets/upload/image/' . $event['gambar']) ?>"
-                                            alt="<?= $event['judul_berita'] ?>" /></a>
-                                </div>
-                                <div class="blog-content">
-                                    <h5 class="blog-title">
-                                        <a href="<?= base_url('berita/read/' . $event['slug_berita']) ?>">
-                                            <?= $event['judul_berita'] ?>
-                                        </a>
-                                    </h5>
-                                    <p class="text">
-                                        Tanggal event :
-                                    </p>
-                                    <span><i class="lni lni-calendar"></i>
-                                        <?= date('M d, Y', strtotime($event['tanggal_event'])) ?></span>
-                                    <p class="text">
-                                        <?= $event['ringkasan'] ?>
-                                    </p>
-
-                                </div>
+<div class="container-fluid p-0">
+    <div class="row no-gutters">
+        <!--====== EVENT ======-->
+        <section class="blog-area pb-5">
+            <div class="container-fluid">
+                <div class="row no-gutters">
+                    <?php foreach ($event as $event) { ?>
+                    <div class="col-lg-4 col-md-8 col-sm-10 p-0">
+                        <div class="single-blog blog-style-one m-0">
+                            <div class="blog-event img-container">
+                                <a href="<?= base_url('berita/read/' . $event['slug_berita']) ?>">
+                                    <img class="img-fluid"
+                                        src="<?= base_url('assets/upload/image/' . $event['gambar']) ?>"
+                                        alt="<?= $event['judul_berita'] ?>" />
+                                    <div class="overlay">
+                                        <div class="overlay-content">
+                                            <h2 class="blog-title">
+                                                <a href="<?= base_url('berita/read/' . $event['slug_berita']) ?>">
+                                                    <?= $event['judul_berita'] ?>
+                                                </a>
+                                            </h2>
+                                            <span><i class="lni lni-calendar"></i>
+                                                <?= date('M d, Y', strtotime($event['tanggal_event'])) ?></span>
+                                        </div>
+                                    </div>
+                                </a>
                             </div>
                         </div>
-                        <?php } ?>
                     </div>
+                    <?php } ?>
                 </div>
-            </section>
-            <!--====== BLOG PART ENDS ======-->
-        </div>
+            </div>
+        </section>
+        <!--====== BLOG PART ENDS ======-->
     </div>
-</section>
+</div>
