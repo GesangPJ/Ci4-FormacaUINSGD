05b8bfaf56b560b93a310baffa858d056f8188de
Gesang Paudra J <g.paudra@gmail.com>
2023-11-24 16:47:10 +0700

    set event image ratio to 4:5

--- app/Views/home/event.php
+++ app/Views/home/event.php
@@ -34,3 +34,22 @@
         <!--====== BLOG PART ENDS ======-->
     </div>
 </div>
+
+<style>
+.blog-event {
+    position: relative;
+}
+
+.img-container {
+    position: relative;
+    overflow: hidden;
+    aspect-ratio: 4/5;
+    /* Set the aspect ratio to 4:5 */
+}
+
+.img-container img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+</style>
